9bc51df9197b934fa773ba7797cc3b8220c97c86
Charles Swaleh <charlswall@live.com>
2023-04-11 20:46:06 +0300

    Created the Vehicles component and /vehicles route

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import NavBar from "./NavBar";
 import HomePage from "./HomePage";
 import { Routes, Route } from "react-router-dom";
 import RouteDetails from "./RouteDetails";
+import Vehicles from "./Vehicles";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/routes/:id" element={<RouteDetails />} />
+        <Route path="/vehicles" element={<Vehicles />} />
       </Routes>
     </div>
   );

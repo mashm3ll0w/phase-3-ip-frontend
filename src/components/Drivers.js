e7891bf2d79fa17bafccb4f43b4237b6cd57e920
Charles Swaleh <charlswall@live.com>
2023-04-12 16:30:37 +0300

    Fetched and displayed driver data from the backend to the Driver component

--- src/components/Drivers.js
+++ src/components/Drivers.js
@@ -1,5 +1,42 @@
-export default function Drivers(){
-    return (
-        <h1>Drivers</h1>
-    )
+import {useState, useEffect} from "react"
+
+export default function Drivers() {
+
+    const [drivers, setDrivers] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:9292/drivers")
+        .then(res => res.json())
+        .then(data => setDrivers(data))
+        .catch(err => console.log("Error :", err.message))
+    }, [])
+
+  return (
+    <div className="main-content container-fluid">
+      <table className="table border-danger table-hover table-striped-columns">
+        <thead className="table-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Current Vehicle</th>
+            <th scope="col">Registration</th>
+            <th scope="col">Origin</th>
+            <th scope="col">Destination</th>
+          </tr>
+        </thead>
+        <tbody>
+            {drivers.map(driver => (
+          <tr key={driver.id}>
+            <th scope="row">{driver.id}</th>
+            <td>{driver.name}</td>
+            <td>{driver.vehicles[0].vehicle_type}</td>
+            <td>{driver.vehicles[0].registration}</td>
+            <td>{driver.routes[0].origin}</td>
+            <td>{driver.routes[0].destination}</td>
+          </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }

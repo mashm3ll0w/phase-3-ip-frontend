107bbdd1e16d31dd369e2222b46e44e4abebb1d8
Charles Swaleh <charlswall@live.com>
2023-04-13 15:52:57 +0300

    Update the drivers component to show only the name of the drivers

--- src/components/Drivers.js
+++ src/components/Drivers.js
@@ -11,17 +11,17 @@ export default function Drivers() {
   }, []);
 
   return (
-    <div className="main-content container-fluid">
+    <div className="main-content container-fluid" style={{"width": "20%"}}>
       <h1>Drivers</h1>
       <table className="table border-danger table-hover table-striped-columns">
         <thead className="table-dark">
           <tr>
             <th scope="col">#</th>
             <th scope="col">Name</th>
-            <th scope="col">Current Vehicle</th>
+            {/* <th scope="col">Current Vehicle</th>
             <th scope="col">Registration</th>
             <th scope="col">Origin</th>
-            <th scope="col">Destination</th>
+            <th scope="col">Destination</th> */}
           </tr>
         </thead>
         <tbody>
@@ -30,10 +30,10 @@ export default function Drivers() {
               <tr key={driver.id}>
                 <th scope="row">{driver.id}</th>
                 <td>{driver.name}</td>
-                <td>{driver.vehicles[0].vehicle_type}</td>
+                {/* <td>{driver.vehicles[0].vehicle_type}</td>
                 <td>{driver.vehicles[0].registration}</td>
                 <td>{driver.routes[0].origin}</td>
-                <td>{driver.routes[0].destination}</td>
+                <td>{driver.routes[0].destination}</td> */}
               </tr>
             ))}
         </tbody>
